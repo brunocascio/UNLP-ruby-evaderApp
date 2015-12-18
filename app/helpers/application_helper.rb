ff7670d20ae58eb96d6a20820bcbc83dd113bcc1
Bruno Cascio <brunocascio@gmail.com>
2015-12-18 19:36:10 -0300

    added helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def is_current_page(section)
     'active' if current_page? section
   end
+
+  def get_classname_as_string(instance)
+    instance.class.name.downcase
+  end
 end
