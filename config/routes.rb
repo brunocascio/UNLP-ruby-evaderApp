1ac069097e9a82aae04f4f8ec9479d082f1f95e4
Bruno Cascio <brunocascio@gmail.com>
2015-12-18 02:19:13 -0300

    Clients Module: 	- Controller 	- Views 	- Helpers 	- Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clients
   resources :people
   resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
