997d73bde7f37648d20c64e95976b30576e2f73b
Bruno Cascio <brunocascio@gmail.com>
2016-01-14 22:57:30 +0000

    Fix #/1

--- app/models/clients_contact.rb
+++ app/models/clients_contact.rb
@@ -1,5 +1,8 @@
 class ClientsContact < ActiveRecord::Base
-  validates :client, :contact, :value, presence: true
+  validates_presence_of :client
+  validates_presence_of :contact
+  validates_presence_of :value
+
   belongs_to :client
   belongs_to :contact
 end
